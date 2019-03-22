f1277837ba0f6b2ace0d3236b18781c37b76cf13
lidonglin <4388193@qq.com>
2019-03-22 17:18:45 +0800

    添加 Submail短信发送支持

---
+++ sms/submail/const.go
@@ -0,0 +1,9 @@
+package submail
+
+const (
+	SUBMAIL_SVC_TIMESTAMP = "https://api.mysubmail.com/service/timestamp"
+
+	SUBMAIL_SMS_SEND_URL        = "https://api.mysubmail.com/message/send"
+	SUBMAIL_SMS_XSEND_URL       = "https://api.mysubmail.com/message/xsend"
+	SUBMAIL_SMS_MULTI_XSEND_URL = "https://api.mysubmail.com/message/multixsend"
+)
---
+++ sms/submail/send.go
@@ -0,0 +1,74 @@
+package submail
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type SmsSend struct {
+	appId    string
+	appKey   string
+	signType string
+
+	to      string
+	content string
+	tag     string
+}
+
+func NewSmsSend(appid, appkey, signtype string) *SmsSend {
+	return &SmsSend{
+		appId:    appid,
+		appKey:   appkey,
+		signType: signtype,
+	}
+}
+
+func (this *SmsSend) SetTo(to string) {
+	this.to = to
+}
+
+func (this *SmsSend) SetContent(content string) {
+	this.content = content
+}
+
+func (this *SmsSend) SetTag(tag string) {
+	this.tag = tag
+}
+
+func (this *SmsSend) Send() (string, error) {
+	config := make(map[string]string)
+
+	config["appid"] = this.appId
+	config["appkey"] = this.appKey
+	config["signType"] = this.signType
+
+	values := url.Values{}
+
+	values.Set("appid", this.appId)
+	values.Set("to", this.to)
+
+	if this.signType != "normal" {
+		timestamp, err := GetTimestamp()
+
+		if err != nil {
+			return "", err
+		}
+
+		values.Set("sign_type", this.signType)
+		values.Set("timestamp", fmt.Sprintf("%d", timestamp))
+		values.Set("sign_version", "2")
+	}
+
+	if this.tag != "" {
+		values.Set("tag", this.tag)
+	}
+
+	signature := caculSign(values, config)
+
+	values.Set("signature", signature)
+
+	//v2 数字签名 content 不参与计算
+	values.Set("content", this.content)
+
+	return HttpPost(SUBMAIL_SMS_SEND_URL, values.Encode())
+}
